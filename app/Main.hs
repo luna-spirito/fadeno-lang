9c8b45f47e5e49e7049c3238d2115c153e18daf2
Gardanta Spirito <guardspirit@protonmail.com>
2024-11-19 20:03:28 +0300

    Fourmolu/style

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,17 @@
 module Main where
 
-import RIO
-import qualified RIO.Text as T
 import Parser
+import RIO
+import RIO.Text qualified as T
 
-render :: ExprT → Text
+render ∷ ExprT → Text
 render = \case
   Lam (Ident name) x → "(\\" <> decodeUtf8Lenient name <> " " <> render x <> ")"
   Let defs final → "(" <> mconcat (defs <&> \(Ident name, val) → "let " <> decodeUtf8Lenient name <> " = " <> render val) <> " in " <> render final <> ")"
-  Op  a x b → "(" <> render a <> " " <> tshow x <> " " <> render b <> ")"
+  Op a x b → "(" <> render a <> " " <> tshow x <> " " <> render b <> ")"
   App a b → "(" <> render a <> " " <> render b <> ")"
   Nat x → tshow x
   Var (Ident x) → decodeUtf8Lenient x
 
-main :: IO ()
+main ∷ IO ()
 main = pure ()
--- app/Parser.hs
+++ app/Parser.hs
@@ -1,13 +1,13 @@
 module Parser where
 
-import RIO hiding (Reader, ask, local)
-import FlatParse.Basic (Parser, satisfyAscii, anyAsciiChar, failed, satisfy, skipMany, byteStringOf, char, string, runParser, Result (..), skipSome, empty, err, Pos, posLineCols, getPos)
-import Prettyprinter (Pretty (..), (<+>), annotate, Doc, line, vsep, indent, defaultLayoutOptions, layoutSmart, hang, softline, nest, group, softline', line')
-import Prettyprinter.Render.Terminal (color, Color (..), AnsiStyle, renderIO, colorDull)
-import Control.Carrier.Writer.Church (WriterC)
-import Control.Carrier.State.Church (StateC, get, evalState, modify)
 import Control.Carrier.Empty.Church (EmptyC, runEmpty)
-import qualified Control.Effect.Empty as E
+import Control.Carrier.State.Church (StateC, evalState, get, modify)
+import Control.Carrier.Writer.Church (WriterC)
+import Control.Effect.Empty qualified as E
+import FlatParse.Basic (Parser, Pos, Result (..), anyAsciiChar, byteStringOf, char, empty, err, failed, getPos, posLineCols, runParser, satisfy, satisfyAscii, skipMany, skipSome, string)
+import Prettyprinter (Doc, Pretty (..), annotate, defaultLayoutOptions, group, hang, indent, layoutSmart, line, line', nest, softline, softline', vsep, (<+>))
+import Prettyprinter.Render.Terminal (AnsiStyle, Color (..), color, colorDull, renderIO)
+import RIO hiding (Reader, ask, local)
 
 -- For now, just untyped.
 
@@ -23,38 +23,41 @@ newtype Ident = Ident ByteString
 
 type Parser' = Parser Pos
 
-space :: Parser' ()
+space ∷ Parser' ()
 space = skipMany (satisfyAscii (\x → x == ' ' || x == '\n'))
 
-token :: Parser' a → Parser' a
+token ∷ Parser' a → Parser' a
 token x = space *> x <* space
 
-digit :: Parser' Word32
-digit = anyAsciiChar >>= \case
-  '0' → pure 0
-  '1' → pure 1
-  '2' → pure 2
-  '3' → pure 3
-  '4' → pure 4
-  '5' → pure 5
-  '6' → pure 6
-  '7' → pure 7
-  '8' → pure 8
-  '9' → pure 9
-  _ → failed
+digit ∷ Parser' Word32
+digit =
+  anyAsciiChar >>= \case
+    '0' → pure 0
+    '1' → pure 1
+    '2' → pure 2
+    '3' → pure 3
+    '4' → pure 4
+    '5' → pure 5
+    '6' → pure 6
+    '7' → pure 7
+    '8' → pure 8
+    '9' → pure 9
+    _ → failed
 
-number :: Parser' Word32
+number ∷ Parser' Word32
 number = token $ foldl' (\acc x → acc + x) 0 <$> some digit
 
-operator :: Parser' OpT
-operator = token $ anyAsciiChar >>= \case
-  '+' → pure Add
-  '-' → pure Sub
-  '*' → pure Mul
-  '/' → pure Div
-  _ → failed
+operator ∷ Parser' OpT
+operator =
+  token $
+    anyAsciiChar >>= \case
+      '+' → pure Add
+      '-' → pure Sub
+      '*' → pure Mul
+      '/' → pure Div
+      _ → failed
 
-ident :: Parser' Ident
+ident ∷ Parser' Ident
 ident = token do
   result ← byteStringOf (skipSome $ satisfy \x → x /= '\\' && x /= ' ' && x /= '\n' && x /= '=' && x /= '(' && x /= ')')
   guard $ not $ result `elem` ["let", "in", "+", "-", "/", "*"]
@@ -69,49 +72,60 @@ data ExprT
   | Var !Ident
   deriving (Show, Eq)
 
-infxr :: Parser' a -> Parser' (a -> a -> a) -> Parser' a
+infxr ∷ Parser' a → Parser' (a → a → a) → Parser' a
 infxr a oper = do
   a' ← a
-  (do
+  ( do
       oper' ← oper
       oper' a' <$> infxr a oper
-    ) <|> pure a'
+    )
+    <|> pure a'
 
-infxl :: Parser' a -> Parser' (a -> a -> a) -> Parser' a
-infxl a oper = a >>= infxl' where
+infxl ∷ Parser' a → Parser' (a → a → a) → Parser' a
+infxl a oper = a >>= infxl'
+ where
   infxl' prev =
     do
-      oper' <- oper
-      next <- a
+      oper' ← oper
+      next ← a
       infxl' $ oper' prev next
-    <|> pure prev
+      <|> pure prev
 
-parsePrim :: Parser' ExprT
-parsePrim = token $
-  Nat <$> number
-  <|> Var <$> ident
-  <|> $(char '(') *> parseTop <* $(char ')')
+parsePrim ∷ Parser' ExprT
+parsePrim =
+  token $
+    Nat
+      <$> number
+        <|> Var
+      <$> ident
+        <|> $(char '(')
+      *> parseTop
+      <* $(char ')')
 
-parseApp :: Parser' ExprT
+parseApp ∷ Parser' ExprT
 parseApp = infxl parsePrim (pure App)
 
-parseMath1 :: Parser' ExprT
+parseMath1 ∷ Parser' ExprT
 parseMath1 =
-  infxr parseApp (operator >>= \case
-    Mul -> pure \x -> Op x Mul
-    Div -> pure \x -> Op x Div
-    _ -> empty
-  )
+  infxr
+    parseApp
+    ( operator >>= \case
+        Mul → pure \x → Op x Mul
+        Div → pure \x → Op x Div
+        _ → empty
+    )
 
-parseMath0 :: Parser' ExprT
+parseMath0 ∷ Parser' ExprT
 parseMath0 =
-  infxr parseMath1 (operator >>= \case
-    Add -> pure \x -> Op x Add
-    Sub -> pure \x -> Op x Sub
-    _ -> empty
-  )
+  infxr
+    parseMath1
+    ( operator >>= \case
+        Add → pure \x → Op x Add
+        Sub → pure \x → Op x Sub
+        _ → empty
+    )
 
-parseLet :: Parser' ExprT
+parseLet ∷ Parser' ExprT
 parseLet = do
   defs ← some do
     token $ $(string "let")
@@ -119,21 +133,25 @@ parseLet = do
     token $ $(char '=')
     expr ← parseTop
     pure (name, expr)
-  (token do
-    $(string "in")
-    val ← parseTop
-    pure $ Let defs val)
+  ( token do
+      $(string "in")
+      val ← parseTop
+      pure $ Let defs val
+    )
 
-parseTop :: Parser' ExprT
-parseTop = token $ parseLet
-  <|> token ($(char '\\')) *> (Lam <$> ident <*> parseTop)
-  <|> parseMath0
-  <|> (err =<< getPos)
+parseTop ∷ Parser' ExprT
+parseTop =
+  token $
+    parseLet
+      <|> token ($(char '\\'))
+      *> (Lam <$> ident <*> parseTop)
+        <|> parseMath0
+        <|> (err =<< getPos)
 
-pIdent :: Ident → Doc AnsiStyle
+pIdent ∷ Ident → Doc AnsiStyle
 pIdent (Ident x) = pretty $ decodeUtf8Lenient x
 
-pOp :: OpT → Doc AnsiStyle
+pOp ∷ OpT → Doc AnsiStyle
 pOp = \case
   Add → "+"
   Sub → "-"
@@ -141,20 +159,20 @@ pOp = \case
   Div → "/"
 
 -- Left/right?
-withPrec :: Int → (Int, Doc ann) → Doc ann
+withPrec ∷ Int → (Int, Doc ann) → Doc ann
 withPrec oldPrec (newPrec, bod) =
   if oldPrec > newPrec
     then "(" <> bod <> ")"
     else bod
 
-complexThreshold :: Int -> Bool
+complexThreshold ∷ Int → Bool
 complexThreshold = (>= 5)
 
-isSimple :: ExprT → Bool
+isSimple ∷ ExprT → Bool
 isSimple =
   let
     ping = do
-      modify @Int (+1)
+      modify @Int (+ 1)
       curr ← get
       if complexThreshold curr
         then E.empty
@@ -166,41 +184,50 @@ isSimple =
       App f a → complexity f *> complexity a
       Nat _ → ping
       Var _ → ping
-  in runIdentity . runEmpty (pure False) (\() → pure True) . evalState @Int 0 . complexity
+   in
+    runIdentity . runEmpty (pure False) (\() → pure True) . evalState @Int 0 . complexity
 
-pExpr :: Int → ExprT → Doc AnsiStyle
-pExpr oldPrec = withPrec oldPrec . \case
-  Lam arg x → (0,
-    let sep = case x of
-          Lam _ _ → " "
-          _ | isSimple x → " "
-            | otherwise → line
-    in annotate (color Magenta) "\\" <> pIdent arg <> sep <> pExpr 0 x)
-  Let defs i -> (1,
-    vsep (defs <&> \(name, val) → annotate (color Cyan) "let" <+> pIdent name <+> annotate (color Cyan) "=" <> softline <> nest 2 (pExpr 0 val))
-    <> line <> annotate (color Cyan) "in" <+> nest 2 (pExpr 1 i))
-  Op a op b →
-    let prec = case op of
-          Add → 2
-          Sub → 2
-          Mul → 3
-          Div → 3
-    in (prec, pExpr prec a <> pOp op <> pExpr prec b)
-  App lam arg → (4, pExpr 4 lam <+> pExpr 5 arg)
-  Nat x → (5, pretty x)
-  Var x → (5, pIdent x)
+pExpr ∷ Int → ExprT → Doc AnsiStyle
+pExpr oldPrec =
+  withPrec oldPrec . \case
+    Lam arg x →
+      ( 0
+      , let sep = case x of
+              Lam _ _ → " "
+              _
+                | isSimple x → " "
+                | otherwise → line
+         in annotate (color Magenta) "\\" <> pIdent arg <> sep <> pExpr 0 x
+      )
+    Let defs i →
+      ( 1
+      , vsep (defs <&> \(name, val) → annotate (color Cyan) "let" <+> pIdent name <+> annotate (color Cyan) "=" <> softline <> nest 2 (pExpr 0 val))
+          <> line
+          <> annotate (color Cyan) "in"
+          <+> nest 2 (pExpr 1 i)
+      )
+    Op a op b →
+      let prec = case op of
+            Add → 2
+            Sub → 2
+            Mul → 3
+            Div → 3
+       in (prec, pExpr prec a <> pOp op <> pExpr prec b)
+    App lam arg → (4, pExpr 4 lam <+> pExpr 5 arg)
+    Nat x → (5, pretty x)
+    Var x → (5, pIdent x)
 
-parse :: ByteString → Either String ExprT
+parse ∷ ByteString → Either String ExprT
 parse inp = case runParser parseTop inp of
   OK x "" → Right x
   Err e → Left $ "Unable to parse at " <> show (posLineCols inp [e])
   _ → Left "Internal error"
 
-parseFile :: FilePath → IO (Either String ExprT)
+parseFile ∷ FilePath → IO (Either String ExprT)
 parseFile x = parse <$> readFileBinary x
 
-parseFileOrDie :: FilePath → IO ExprT
+parseFileOrDie ∷ FilePath → IO ExprT
 parseFileOrDie x = fromRight (error "parsing failed") <$> parseFile x
 
-printExpr :: ExprT → IO ()
+printExpr ∷ ExprT → IO ()
 printExpr expr = renderIO stdout $ layoutSmart defaultLayoutOptions $ pExpr 0 expr <> line
